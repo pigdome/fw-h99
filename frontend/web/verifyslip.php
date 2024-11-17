7a575bda9935fddf292a61deab40d0f017a1b463
apirak <apirak@punsarn.asia>
2024-11-17 15:55:49 +0000

    verify slip: check amount

--- frontend/web/verifyslip.php
+++ frontend/web/verifyslip.php
@@ -5,6 +5,7 @@ $endpoint = "https://www.tmweasy.com/api_verify_slip.php";
 
 $qrcode = $_REQUEST['qrcode'];
 $ref1  = $_REQUEST['ref1'];
+$amount = $_REQUEST['amount'];
 
 $params = array('username' => 'huay6666', 'password' => 'Met159753xx.', 'qrcode' => $qrcode, 'focus_bank' => '006', 'ip' => '49.229.127.232', 'ref1' => $ref1);
 
@@ -14,13 +15,22 @@ $json = file_get_contents($url);
 
 $object = json_decode($json);
 if ($object->status == 1) {
+  if ($object->amout != $amount) {
+    $status = "❌ ไม่ถูกต้อง";
+  } else {
+    $status = "✅ ถูกต้อง";
+  }
   $msg =
-    "ผลการตรวจสอบสลิป: " . $object->msg .
+    "ผลการตรวจสอบ: " . $status .
+    "\nสถานะการชำระเงิน: " . $object->msg .
     "\nเวลา: " . $object->slip_time .
     "\nชื่อบัญชี: " . $object->sender_data->name .
     "\nเลขที่บัญชี: " . $object->sender_data->acc_no .
     "\nธนาคาร: " . $object->sender_data->bank_name .
-    "\nจำนวนเงิน: " . $object->amount;
+    "\nจำนวนเงินที่ได้รับ: " . $object->amount .
+    "\nยอดแจ้งฝากเงิน(credit): " . $amount .
+    "\nref1: " . $ref1;
+
 
   $bot_api_token = '7840375811:AAG-Qa6vlyFstnBaphfAFMd-cG7BkY-IjgY';
   $chat_id = '-4593520082';
